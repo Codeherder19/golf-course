30474c2ee3ac67b00a90afcd391c25cef9bfdb45
Brian Forbes <brianandrewf81@gmail.com>
2021-05-05 15:41:33 -0600

    Complete 11 tests for golf-course

--- golf-course.js
+++ golf-course.js
@@ -4,7 +4,17 @@ class GolfCourse {
     this.difficulty = difficulty;
     this.openings = openings;
     this.features = features;
+    this.currentlyPlaying = []
+  }
+  checkInGroup(party) {
+    if (party.length <= this.openings) {
+    for (var i = 0; i < party.length; i++) {
+      this.currentlyPlaying.unshift(party[i].name);
+      this.openings -= party.length;
+      }
+    } console.log(this.currentlyPlaying)
   }
 }
+
 
 module.exports = GolfCourse;
